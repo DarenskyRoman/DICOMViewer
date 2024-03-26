6dad9f1c72702ddccd012ca345259584445f9b59
DarenskyRoman <darensky.roman.k@gmail.com>
2024-03-26 08:34:40 +0400

    i hate this very much

--- logic.py
+++ logic.py
@@ -1,7 +1,31 @@
 from pydicom import dcmread
+from pathlib import Path
+import numpy as np
 
-def read(file):
-    try:
-        return dcmread(file)
-    except Exception:
-        return
+def read(dir):
+    dir = Path(dir)
+    volumes = {}
+    bodyParts = set()
+    for patient_dir in dir.iterdir():
+        if patient_dir.is_dir():
+            for file in patient_dir.iterdir():
+                try:
+                    dicom_file = dcmread(file, force=True)
+                    body_part = dicom_file.get('BodyPartExamined')
+                    if body_part not in bodyParts:
+                        bodyParts.add(body_part)
+                        volumes[f"{body_part}"] = []
+
+                    pixel_data = dicom_file.get('PixelData')
+
+                    if pixel_data is None:
+                        print("Секция Pixel Data отсутствует в файле:", file)
+                        continue
+
+                    pixel_array = np.frombuffer(pixel_data, dtype=np.uint16)
+
+                    volumes[f"{body_part}"].append(pixel_array.reshape(int(dicom_file.Rows), int(dicom_file.Columns)))
+
+                except Exception as e:
+                    print(f"Ошибка при обработке файла {file}: {e}")
+    return volumes
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QListWidgetItem
 from PyQt6 import uic
-from logic import read
+import numpy as np
+import logic
 
 
 class UI(QMainWindow):
@@ -12,31 +14,69 @@ class UI(QMainWindow):
 
         uic.loadUi("silly.ui", self)
         
-        self.figure = plt.figure()
-  
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-          
-        self.imageLayout.addWidget(self.toolbar)
-        self.imageLayout.addWidget(self.canvas)
+        self.axialFigure = plt.figure()  
+        self.axialCanvas = FigureCanvas(self.axialFigure)
+        self.axialToolbar = NavigationToolbar(self.axialCanvas, self)
+        self.axialLayout.addWidget(self.axialCanvas)
+        self.axialLayout.addWidget(self.axialToolbar)
 
+        self.sagittalFigure = plt.figure()
+        self.sagittalCanvas = FigureCanvas(self.sagittalFigure)
+        self.sagittalToolbar = NavigationToolbar(self.sagittalCanvas, self)
+        self.sagittalLayout.addWidget(self.sagittalCanvas)
+        self.sagittalLayout.addWidget(self.sagittalToolbar)
+
+        self.coronalFigure = plt.figure()
+        self.coronalCanvas = FigureCanvas(self.coronalFigure)
+        self.coronalToolbar = NavigationToolbar(self.coronalCanvas, self)
+        self.coronalLayout.addWidget(self.coronalCanvas)
+        self.coronalLayout.addWidget(self.coronalToolbar)
+
+        
         self.openFile.triggered.connect(self.fileSearch)
+        self.bodyPartsList.itemDoubleClicked.connect(self.bodyPartSelection)
 
+
+    def bodyPartSelection(self, bodyPart):
+        self.axialFigure.clear()
+        #self.sagittalFigure.clear()
+        #self.coronalFigure.clear()
+        slices = self.volumes[f"{bodyPart.text()}"]
+        
+        self.axAxial = self.axialFigure.add_subplot(111)
+        self.axAxial.imshow(slices[0], cmap=plt.cm.bone)
+        self.axialCanvas.draw()
+        #self.axSagitall = self.sagittalFigure.add_subplot()
+        #axCoronal = self.coronalFigure.add_subplot()
+        
+        self.axialSlider = Slider(ax=self.axialFigure.add_subplot(111),
+                                  label="Slice",
+                                  valmin=0,
+                                  valinit=0,
+                                  valstep=1,
+                                  valmax=len(slices)-1)
+        self.axialSlider.on_changed(lambda val: self.plotAxialSLice(val, slices))
+
+    def plotAxialSLice(self, val, slices):
+        self.axAxial.imshow(slices[val], cmap=plt.cm.bone)
+        self.axialCanvas.draw()
 
     def fileSearch(self):
-        file = QFileDialog.getOpenFileName(caption = "Open file", filter="DICOM files (*.dcm)")
-        filedata = read(file[0])
+        dir = QFileDialog.getExistingDirectory(caption="Select directory")
+        self.volumes = logic.read(dir)
+        for bodyPart in self.volumes.keys():
+            self.bodyPartsList.addItem(f"{bodyPart}")
 
-        try:
-            self.figure.clear()
+        # try:
+        #     self.figure.clear()
     
-            ax = self.figure.add_subplot()
-            ax.imshow(filedata.pixel_array, cmap=plt.cm.bone)
+        #     ax = self.figure.add_subplot()
+        #     ax.imshow(filedata.pixel_array, cmap=plt.cm.bone)
             
-            self.canvas.draw()
+        #     self.canvas.draw()
 
-        except Exception:
-            self.errorWindow("Cannot open this file")
+        # except Exception:
+        #     self.errorWindow("Cannot open this file")
 
     def errorWindow(self, text):
         QMessageBox.critical(self, "Error", text)
