47846966dab3190434478f8648df39c3b2e0ae50
DarenskyRoman <darensky.roman.k@gmail.com>
2024-03-27 02:01:19 +0400

    im tired

--- logic.py
+++ logic.py
@@ -5,27 +5,24 @@ import numpy as np
 def read(dir):
     dir = Path(dir)
     volumes = {}
-    bodyParts = set()
     for patient_dir in dir.iterdir():
         if patient_dir.is_dir():
             for file in patient_dir.iterdir():
                 try:
-                    dicom_file = dcmread(file, force=True)
-                    body_part = dicom_file.get('BodyPartExamined')
-                    if body_part not in bodyParts:
-                        bodyParts.add(body_part)
+                    dc = dcmread(file, force=True)
+                    body_part = dc.get('BodyPartExamined')
+                    if body_part not in volumes.keys():
                         volumes[f"{body_part}"] = []
 
-                    pixel_data = dicom_file.get('PixelData')
+                    pixel_data = dc.get('PixelData')
 
                     if pixel_data is None:
-                        print("Секция Pixel Data отсутствует в файле:", file)
+                        print("Pixel Data not found:", file)
                         continue
 
                     pixel_array = np.frombuffer(pixel_data, dtype=np.uint16)
-
-                    volumes[f"{body_part}"].append(pixel_array.reshape(int(dicom_file.Rows), int(dicom_file.Columns)))
+                    volumes[f"{body_part}"].append(pixel_array.reshape(int(dc.Rows), int(dc.Columns)))
 
                 except Exception as e:
-                    print(f"Ошибка при обработке файла {file}: {e}")
+                    print(f"Error with file {file}: {e}")
     return volumes
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
-from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QListWidgetItem
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6 import uic
-import numpy as np
 import logic
+from matplotlib.figure import Figure
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, width, height):
+        self.fig = Figure(figsize=(width, height))
+        self.axes = self.fig.add_subplot(111)
+        super(MplCanvas, self).__init__(self.fig)
+
+    def addSlider(self, valmax):
+        ax = self.fig.add_axes([0.25, 0.1, 0.65, 0.03])
+        self.slider = Slider(ax=ax,
+                            label="Slice",
+                            valmin=0,
+                            valinit=0,
+                            valstep=1,
+                            valmax=valmax)
 
 
 class UI(QMainWindow):
@@ -14,23 +30,20 @@ class UI(QMainWindow):
 
         uic.loadUi("silly.ui", self)
         
-        self.axialFigure = plt.figure()  
-        self.axialCanvas = FigureCanvas(self.axialFigure)
-        self.axialToolbar = NavigationToolbar(self.axialCanvas, self)
+        self.axialCanvas = MplCanvas(1, 1)
+        axialToolbar = NavigationToolbar2QT(self.axialCanvas, self)
         self.axialLayout.addWidget(self.axialCanvas)
-        self.axialLayout.addWidget(self.axialToolbar)
+        self.axialLayout.addWidget(axialToolbar)
 
-        self.sagittalFigure = plt.figure()
-        self.sagittalCanvas = FigureCanvas(self.sagittalFigure)
-        self.sagittalToolbar = NavigationToolbar(self.sagittalCanvas, self)
+        self.sagittalCanvas = MplCanvas(1, 1)
+        sagittalToolbar = NavigationToolbar2QT(self.sagittalCanvas, self)
         self.sagittalLayout.addWidget(self.sagittalCanvas)
-        self.sagittalLayout.addWidget(self.sagittalToolbar)
+        self.sagittalLayout.addWidget(sagittalToolbar)
 
-        self.coronalFigure = plt.figure()
-        self.coronalCanvas = FigureCanvas(self.coronalFigure)
-        self.coronalToolbar = NavigationToolbar(self.coronalCanvas, self)
+        self.coronalCanvas = MplCanvas(1, 1)
+        coronalToolbar = NavigationToolbar2QT(self.coronalCanvas, self)
         self.coronalLayout.addWidget(self.coronalCanvas)
-        self.coronalLayout.addWidget(self.coronalToolbar)
+        self.coronalLayout.addWidget(coronalToolbar)
 
         
         self.openFile.triggered.connect(self.fileSearch)
@@ -38,27 +51,19 @@ class UI(QMainWindow):
 
 
     def bodyPartSelection(self, bodyPart):
-        self.axialFigure.clear()
-        #self.sagittalFigure.clear()
-        #self.coronalFigure.clear()
+        self.axialCanvas.axes.cla()
+        #self.sagittalCanvas.clear()
+        #self.coronalCanvas.clear()
+
         slices = self.volumes[f"{bodyPart.text()}"]
         
-        self.axAxial = self.axialFigure.add_subplot(111)
-        self.axAxial.imshow(slices[0], cmap=plt.cm.bone)
-        self.axialCanvas.draw()
-        #self.axSagitall = self.sagittalFigure.add_subplot()
-        #axCoronal = self.coronalFigure.add_subplot()
-        
-        self.axialSlider = Slider(ax=self.axialFigure.add_subplot(111),
-                                  label="Slice",
-                                  valmin=0,
-                                  valinit=0,
-                                  valstep=1,
-                                  valmax=len(slices)-1)
-        self.axialSlider.on_changed(lambda val: self.plotAxialSLice(val, slices))
+        self.axialCanvas.addSlider(len(slices)-1)
+        self.axialCanvas.slider.on_changed(lambda val: self.plotAxialSLice(val, slices))
+        self.plotAxialSLice(0, slices)
+        #self.axialLayout.addWidget(slider)
 
     def plotAxialSLice(self, val, slices):
-        self.axAxial.imshow(slices[val], cmap=plt.cm.bone)
+        self.axialCanvas.axes.imshow(slices[val], cmap=plt.cm.bone)
         self.axialCanvas.draw()
 
     def fileSearch(self):
@@ -67,22 +72,9 @@ class UI(QMainWindow):
         for bodyPart in self.volumes.keys():
             self.bodyPartsList.addItem(f"{bodyPart}")
 
-        # try:
-        #     self.figure.clear()
-    
-        #     ax = self.figure.add_subplot()
-        #     ax.imshow(filedata.pixel_array, cmap=plt.cm.bone)
-            
-        #     self.canvas.draw()
+if __name__ == "__main__": 
 
-        # except Exception:
-        #     self.errorWindow("Cannot open this file")
-
-    def errorWindow(self, text):
-        QMessageBox.critical(self, "Error", text)
-
-
-app = QApplication([])
-window = UI()
-window.show()
-app.exec()
+    app = QApplication([])
+    window = UI()
+    window.show()
+    app.exec()
